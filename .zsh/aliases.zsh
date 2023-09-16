4cc7c164cea0e7d45a383aa79d6426e0d0018e56
Levon Kirakosyan <noosx.e@gmail.com>
2023-09-16 15:27:31 -0700

    Use eza.

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,3 +1,11 @@
-alias tree='exa --tree --level=2'
+alias tree='exa --tree --level=2eza_params=('--git' '--icons' '--classify' '--group-directories-first' '--time-style=long-iso' '--group' '--color-scale')'
+alias ls='eza $eza_params'
+alias l='eza --git-ignore $eza_params'
+alias ll='eza --all --header --long $eza_params'
+alias llm='eza --all --header --long --sort=modified $eza_params'
+alias la='eza -lbhHigUmuSa'
+alias lx='eza -lbhHigUmuSa@'
+alias lt='eza --tree $eza_params'
+alias tree='eza --tree $eza_params'
 alias s="kitty +kitten ssh"
 alias gitl="git log --oneline --graph"
